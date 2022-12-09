c505c686746b684ae1c286c58bdd438374d50f6f
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-09 08:56:57 +0100

    UDP server example

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -1,20 +1,34 @@
 package com.tg;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class Main {
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(5000)) {
-            while (true) {
-//                Socket socket =serverSocket.accept();
-//                Echoer echoer = new Echoer(socket);
-//                echoer.start();
-                new Echoer(serverSocket.accept()).start(); // this line replaces the top three commented lines
-            }
 
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(5000);
+
+            while (true) {
+                byte[] buffer = new byte[50];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                System.out.println("Text received is: " + new String(buffer, 0, packet.getLength()));
+
+                String returnString = "echo: " + new String(buffer, 0, packet.getLength());
+                byte[] buffer2 = returnString.getBytes();
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                packet = new DatagramPacket(buffer2, buffer2.length, address, port);
+                socket.send(packet);
+            }
+        } catch (SocketException e) {
+            System.out.println("SocketException: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("Server exception " + e.getMessage());
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 }
